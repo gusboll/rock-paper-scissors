cc26868a11f94272591c08d5c6cd5a776f82d7d8
gusboll <augusto.sboll@gmail.com>
2025-04-27 17:44:36 -0300

    work on the UI of the game

--- script1.js
+++ script1.js
@@ -1,66 +1,104 @@
-alert("This is a best of 5");
-
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 10);
     let computerChoice = "";
     if (random <= 3) {
         computerChoice = "Rock";
-        console.log("Rock");
     } else if (random >= 3 && random <= 6) {
         computerChoice = "Paper";
-        console.log("Paper");
     } else if (random >= 7) {
         computerChoice = "Scissors";
-        console.log("Scissors");
     }
     return computerChoice;
 }
 
-function getHumanChoice() {
-    let humanChoice = "";
-    humanChoice = prompt("Paper, Rock or Scissors?")
-    console.log(humanChoice);
-    return humanChoice;
-}
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+let humanChoice = "";
+rock.addEventListener("click", () => {
+    humanChoice = "Rock";
+    playRound(humanChoice);
+})
+
+paper.addEventListener("click", () => {
+    humanChoice = "Paper";
+    playRound(humanChoice);
+});
+
+scissors.addEventListener("click", () => {
+    humanChoice = "Scissors";
+    playRound(humanChoice);
+});
+
 
 let humanScore = 0;
 let computerScore = 0;
 
+function resetGame(){
+    window.location.reload();
+}
+
+function checkWinner(){
+    if (computerScore + humanScore >= 5){
+        if (humanScore > computerScore){
+            alert("Congratulations! You won against the mighty machine!" + " You won " + humanScore + " times!");
+            if (confirm("You want to play again?")){
+                resetGame();
+            } else {
+                txt = "You pressed cancel!";
+            }
+        } else if (computerScore > humanScore){
+            alert("Oh no! The cruel machine won this game!" + " The machine won " + computerScore + " times!");
+            if (confirm("You want to play again?")){
+                resetGame();
+            } else {
+                txt = "You pressed cancel!";
+            }
+        }
+    }
+}
+
 function playRound(humanChoice, computerChoice){
     computerChoice = getComputerChoice();
-    humanChoice = getHumanChoice();
     if (humanChoice === computerChoice){
-        console.log("Draw!");
+        alert("Draw!");
     } else if (
         (humanChoice === "Rock" && computerChoice === "Scissors") ||
         (humanChoice === "Paper" && computerChoice === "Rock") ||
         (humanChoice === "Scissors" && computerChoice === "Paper")
  ) {
-        console.log("You won! " + humanChoice + " beats " + computerChoice);
+        alert("You won! " + humanChoice + " beats " + computerChoice);
         humanScore = (humanScore + 1);
     } else {
-        console.log("You lost! " + computerChoice + " beats " + humanChoice);
+        alert("You lost! " + computerChoice + " beats " + humanChoice);
         computerScore = (computerScore + 1);
     }
-    console.log("Your score is " + humanScore);
-    console.log("Computer score is " + computerScore);
+    document.querySelector(".playerwins").textContent = "You have won " + humanScore + " times!";
+    document.querySelector(".computerwins").textContent = "The computer has won " + computerScore + " times!";
+    checkWinner();
     return computerScore, humanScore;
 }
 
-function playGame(){
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    if (humanScore > computerScore){
-        alert("Congratulations! You won against the mighty machine!" + " You won " + humanScore + " times!");
-    } else{
-        alert("Oh no! The cruel machine won this game!" + " The machine won " + computerScore + " times!");
-    }
-}
 
-playGame();
+
+
+
+
+// function playGame(){
+//     playRound();
+//     playRound();
+//     playRound();
+//     playRound();
+//     playRound();
+//     if (humanScore > computerScore){
+//         alert("Congratulations! You won against the mighty machine!" + " You won " + humanScore + " times!");
+//     } else{
+//         alert("Oh no! The cruel machine won this game!" + " The machine won " + computerScore + " times!");
+//     }
+// }
+
+// playGame();
 
 
 
